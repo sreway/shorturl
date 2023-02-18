f137ad57084bae97e7817f722be129ea010f335f
Andrey Oleynik <a.y.oleynik@gmail.com>
2023-02-18 14:03:01 +0300

    change check signals via notify context

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,7 +5,6 @@ import (
 	"os"
 	"os/signal"
 	"sync"
-	"syscall"
 
 	"golang.org/x/exp/slog"
 
@@ -16,59 +15,53 @@ import (
 )
 
 func main() {
-	var (
-		err  error
-		code int
-	)
+	var code int
 
 	log := slog.New(slog.NewJSONHandler(os.Stdout).
 		WithAttrs([]slog.Attr{slog.String("service", "main")}))
 
 	log.Info("start app")
 
-	cfg, err := config.New()
-	if err != nil {
-		log.Error("failed initialize config", err)
-	}
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer func() {
+		stop()
+		os.Exit(code)
+	}()
 
 	wg := new(sync.WaitGroup)
 	wg.Add(1)
-
-	ctx, cancel := context.WithCancel(context.Background())
-
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	exit := make(chan int)
-
-	repoURL := repo.New()
-	service := shortener.New(repoURL, &cfg.Server.ShortURL)
-	srv := http.New(service)
 
 	go func() {
 		defer wg.Done()
+
+		cfg, err := config.New()
+		if err != nil {
+			log.Error("failed initialize config", err)
+			stop()
+			exit <- 1
+			return
+		}
+		repoURL := repo.New()
+		service := shortener.New(repoURL, &cfg.Server.ShortURL)
+		srv := http.New(service)
+
 		err = srv.Run(ctx, &cfg.Server.HTTP)
 		if err != nil {
 			log.Error("failed run delivery", err)
-			signals <- syscall.SIGSTOP
+			stop()
+			exit <- 1
+			return
 		}
 	}()
 
 	go func() {
-		for {
-			s := <-signals
-			switch s {
-			case syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
-				log.Info("trigger graceful shutdown app")
-				exit <- 0
-			default:
-				log.Info("trigger shutdown app")
-				exit <- 1
-			}
-		}
+		<-ctx.Done()
+		stop()
+		exit <- 0
+		log.Info("trigger graceful shutdown app")
 	}()
 
 	code = <-exit
-	cancel()
 	wg.Wait()
-	os.Exit(code)
 }

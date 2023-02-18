b25c39a706128ebeb90d7e51b9cecdf36d1a99be
Andrey Oleynik <a.y.oleynik@gmail.com>
2023-02-18 17:36:57 +0300

    fix config parse env tags

--- internal/config/config.go
+++ internal/config/config.go
@@ -23,8 +23,8 @@ type (
 	}
 
 	config struct {
-		http     http
-		shortURL shortURL
+		http     *http
+		shortURL *shortURL
 	}
 
 	http struct {
@@ -39,11 +39,11 @@ type (
 )
 
 func (c *config) HTTP() *http {
-	return &c.http
+	return c.http
 }
 
 func (c *config) ShortURL() *shortURL {
-	return &c.shortURL
+	return c.shortURL
 }
 
 func (h *http) GetScheme() string {
@@ -63,10 +63,20 @@ func (s *shortURL) GetCounter() uint64 {
 }
 
 func NewConfig() (*config, error) {
+	cfgHTTP := new(http)
+	cfgShortURL := new(shortURL)
 	cfg := new(config)
-	if err := env.Parse(cfg); err != nil {
+
+	if err := env.Parse(cfgHTTP); err != nil {
 		return nil, err
 	}
+
+	if err := env.Parse(cfgShortURL); err != nil {
+		return nil, err
+	}
+
+	cfg.http = cfgHTTP
+	cfg.shortURL = cfgShortURL
 	return cfg, nil
 }
 

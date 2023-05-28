37d6f6747322b5619a7581b353b235e057f42df6
Andrey Oleynik <a.y.oleynik@gmail.com>
2023-05-29 00:40:07 +0300

    update encode/decode base62 func

--- internal/usecases/shortener/base62.go
+++ internal/usecases/shortener/base62.go
@@ -3,25 +3,45 @@ package shortener
 import (
 	"fmt"
 	"math/big"
+	"strings"
 )
 
+const base62Chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 func encodeUUID(uuid [16]byte) string {
-	var i big.Int
-	i.SetBytes(uuid[:])
-	return i.Text(62)
+	num := big.NewInt(0).SetBytes(uuid[:])
+	base62 := big.NewInt(62)
+	remainder := big.NewInt(0)
+
+	var buf [22]byte
+	n := len(buf)
+
+	for num.Cmp(big.NewInt(0)) > 0 {
+		num.DivMod(num, base62, remainder)
+		n--
+		buf[n] = base62Chars[remainder.Int64()]
+	}
+	return string(buf[n:])
 }
 
 func decodeUUID(s string) ([]byte, error) {
-	var i big.Int
-	_, ok := i.SetString(s, 62)
-	if !ok {
-		return nil, fmt.Errorf("cannot parse base62: %q", s)
-	}
-	var uuid []byte
-	copy(uuid, i.Bytes())
+	num := big.NewInt(0)
+	base62 := big.NewInt(62)
 
-	if len(i.Bytes()) < 16 {
-		return nil, fmt.Errorf("invalid UUID length: %d", len(i.Bytes()))
+	for _, char := range s {
+		index := strings.IndexByte(base62Chars, byte(char))
+		if index == -1 {
+			return nil, fmt.Errorf("invalid base62 character: %c", char)
+		}
+		num.Mul(num, base62)
+		num.Add(num, big.NewInt(int64(index)))
 	}
-	return i.Bytes(), nil
+
+	uuidBytes := num.Bytes()
+
+	if len(uuidBytes) < 16 {
+		return nil, fmt.Errorf("invalid UUID length: %d", len(uuidBytes))
+	}
+
+	return uuidBytes, nil
 }
